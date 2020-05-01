753651ba18bb0f66436e2fe62f534fde4f436303
Sayed Sajid Ali <sajidlpu1@gmail.com>
2020-05-01 12:37:22 +0530

    Updates

---
+++ precache-manifest.f3e35a51155395ff41afb5250aad6d63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c6d3e2418b917699b5b92f68b624357",
+    "url": "/ToDo-React-App/index.html"
+  },
+  {
+    "revision": "35672b8743106f8ba5a5",
+    "url": "/ToDo-React-App/static/css/main.52258080.chunk.css"
+  },
+  {
+    "revision": "da2d2207484d0c81bbc4",
+    "url": "/ToDo-React-App/static/js/2.7811022d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-React-App/static/js/2.7811022d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35672b8743106f8ba5a5",
+    "url": "/ToDo-React-App/static/js/main.8dc9a8e2.chunk.js"
+  },
+  {
+    "revision": "da9c36dc45ff701304d9",
+    "url": "/ToDo-React-App/static/js/runtime-main.42703bb0.js"
+  },
+  {
+    "revision": "fb6657f9cfc77a1bc081faca9eb0eeb3",
+    "url": "/ToDo-React-App/static/media/logo.fb6657f9.png"
+  }
+]);
